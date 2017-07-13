1d478d7db5ea4abadc5854f8a8d8b60d0606904d
tejaskb04 <tejaskb@gmail.com>
2017-07-12 22:03:23 -0700

    Home Page Template
    Added bootstrap, jquery, img files and css resources not loading atm

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 var express = require("express");
 var app = express();
 var path = require("path");
+app.use("/css", express.static(__dirname + "/node_modules/bootstrap/dist/css")); // bootstrap css include
+app.use("/js", express.static(__dirname + "/node_modules/bootstrap/dist/js")); // bootstrap js include
+app.use("/js", express.static(__dirname + "/node_modules/jquery/dist")); // jquery include
+/* *****
+Image Includes:
+***** */
+app.use(express.static(__dirname + "/home_library/img"));
 app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname + "/index.html"));
+    res.sendFile(path.join(__dirname + "/home_library/index.html"));
 });
 app.listen(8080);
