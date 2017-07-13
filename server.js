0a7df59c8bde3ce2cf2db0a5e0923e771e81b94d
tejaskb04 <tejaskb@gmail.com>
2017-07-13 09:39:24 -0700

    Home Page Functional
    All dependencies loaded

--- server.js
+++ server.js
@@ -1,14 +1,9 @@
 var express = require("express");
-var app = express();
 var path = require("path");
-app.use("/css", express.static(__dirname + "/node_modules/bootstrap/dist/css")); // bootstrap css include
-app.use("/js", express.static(__dirname + "/node_modules/bootstrap/dist/js")); // bootstrap js include
-app.use("/js", express.static(__dirname + "/node_modules/jquery/dist")); // jquery include
-/* *****
-Image Includes:
-***** */
-app.use(express.static(__dirname + "/home_library/img"));
+var app = express();
+app.use(express.static("home_library"));
 app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname + "/home_library/index.html"));
+    console.log(req.url);
+    res.sendFile(path.join(__dirname, "/home_library/index.html"));
 });
 app.listen(8080);
