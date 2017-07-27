ac07d4150f63073e9c945eff836cbdd079911caf
tejaskb04 <tejaskb@gmail.com>
2017-07-26 18:11:20 -0700

    Memez

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 var express = require("express");
 var path = require("path");
+//var bodyparser = require("body-parser");
 var app = express();
+//app.use(bodyparser.json());
 app.use(express.static("home_library"));
-app.get("/", function (req, res) {
-    console.log(req.url);
+
+app.get("/index", function(req, res) {
     res.sendFile(path.join(__dirname, "/home_library/index.html"));
+});
+app.get("/evaluate", function(req, res) {
+    req.body.foreach(function(i) {
+        
+    });
+});
+app.get("https://management.azure/com/", function(req, res) {
+    // POST REQUEST to frontend
+    console.log(req.url);
 });
 app.listen(8080);
